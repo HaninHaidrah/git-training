bf367a533950c4fbc4375ea2cf53606243b2ff8d
Hanin Haidrah <haneenhadey@gmail.com>
2023-03-18 21:55:02 +0300

    Refactor power function

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { parseOperands } = require("./utils");
 
 const app = express();
 
 app.get("/power", (req, res) => {
-  const { x, y } = req.query;
+  const { x, y } = parseOperands(req.query);
   const power = x ** y;
   res.send(200, power);
 });
